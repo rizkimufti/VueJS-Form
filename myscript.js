33b7c4cf5cb6547953d9f76c82dbadf8705b1d83
rizkimufti <you@example.com>
2018-01-03 14:55:04 +0700

    deploy

---
+++ myscript.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '.container',
+    data: {
+        name: 'your name',
+        role: 'Full stack Developer',
+        email: 'email@example.com',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget luctus enim. Sed nec mauris a mi hendrerit dictum vel vel nisl. Phasellus ac pharetra lacus. Nam suscipit orci non ornare aliquam. Morbi sollicitudin venenatis consectetur. Praesent rutrum magna vitae nisl viverra, ut auctor purus ultrices. Vestibulum eu pellentesque orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse ligula dolor, mollis ac nibh non, rhoncus interdum arcu. Vestibulum euismod, leo nec egestas blandit, justo eros tempor ex, vitae sagittis odio lectus at diam. Maecenas nec feugiat erat. ',
+        submit: false,
+    }
+  })
